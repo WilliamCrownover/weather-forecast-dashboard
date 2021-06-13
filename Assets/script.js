ea19b76130b71f98c82033d165267cae7af1fea5
William Crownover <williamcrownover1@gmail.com>
2021-06-12 22:14:34 -0700

    Handled a successful api request with lat and lon parameters

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,64 @@
 // GLOBAL VARIABLES
 // API
 var APIKey = "42047833fa93ab4b60ef3a5ff26f282d";
-var locQueryUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+var APIURL = "http://api.openweathermap.org/data/2.5/";
 
 // Elements
 var searchFormEl = $("#searchForm");
+var weatherContainerEl = $("#weatherContainer");
 
 // -------------------------------------------------------------------------------
 // FUNCTIONS
 
-function searchApiByCity(query) {
-    locQueryUrl += `${query}&appid=${APIKey}`;
+function displayNotFound() {
+    console.log("here");
+    weatherContainerEl.append(`
+        <h3>No Results Found. Please Try Again</h3>
+    `);
+}
+
+function displayContent(weatherData) {
+    
+}
+
+function searchApiByCoordinates(lat, lon) {
+    var locQueryUrl = `${APIURL}onecall?${lat}&${lon}&exclude=minutely,hourly&units=imperial&appid=${APIKey}`;
 
     fetch(locQueryUrl)
         .then(function (response) {
+            if(!response.ok) {
+                displayNotFound();
+                throw response.json();
+            }
             return response.json();
         })
         .then(function (locRes) {
             console.log("~ locRes", locRes);
+            displayContent(locRes);
+        })
+        .catch(function (error) {
+            return error;
+        });
+}
+
+function searchApiByCity(query) {
+    var locQueryUrl = `${APIURL}weather?q=${query}&appid=${APIKey}`;
+
+    fetch(locQueryUrl)
+        .then(function (response) {
+            if(!response.ok) {
+                displayNotFound();
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (locRes) {
+            var cityLat = `lat=${locRes.coord.lat}`;
+            var cityLon = `lon=${locRes.coord.lon}`;
+            searchApiByCoordinates(cityLat, cityLon);
+        })
+        .catch(function (error) {
+            return error;
         });
 }
 
